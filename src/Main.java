8ac698cca78b9f9a9adbbb2aa7d919b346dd3fff
Natalia Novohatskaja <natalinovohatskaja@gmail.com>
2023-03-08 12:18:06 +0300

    fixed bugs

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,6 @@ public class Main {
         System.out.println(text.hasWord("282006"));
 
 
-        }
-
     }
+
+}
--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,16 +1,14 @@
 import java.util.*;
+
 public class WordsChecker {
-        protected String text;
-        Set<String> set;
+    private String text;
+    private Set<String> set;
 
-
-        public WordsChecker(String text) {
-            this.text = text;
-            set = new HashSet<>(List.of(text.split("\\P{IsAlphabetic}+")));
-
-        }
-
-        public boolean hasWord(String say) {
-            return set.contains(say);
-        }
+    public WordsChecker(String text) {
+        this.text = text;
+        set = new HashSet<>(List.of(text.split("\\P{IsAlphabetic}+")));
+    }
+    public boolean hasWord(String say) {
+        return set.contains(say);
+    }
 }
